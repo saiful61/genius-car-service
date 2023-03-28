105e12412a1dce5796043c0f8dcbda94367e6a53
unknown <muhammadsaifulislam17@gmail.com>
2023-03-28 15:53:44 +0600

    final-push

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 const About = () => {
     return (
-        <div style={{ minHeight: "100Vh" }} className='container mt-2 '>
-            <h2 className='text-center'>This is about us</h2>
-        </div>
+    <div className='container mt-5'>
+        <Form>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label>Name</Form.Label>
+          <Form.Control type="Name" placeholder="Name" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="name@example.com" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+          <Form.Label>Message</Form.Label>
+          <Form.Control as="textarea" rows={3} />
+        </Form.Group>
+      </Form>
+      <Button className='mb-5' variant="primary">Submit</Button>
+    </div>
     );
 };
 
